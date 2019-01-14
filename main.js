fe27df350ce7941c0ab339ead3c5893bea47c29a
SophiaSheref <sophia.sheref@gmail.com>
2019-01-14 09:20:04 -0500

    added developer tools and time for clockin

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path = require('path');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let resetPasswordWindow;
 
 //Listen for app to be ready
 app.on('ready', function(){
@@ -16,6 +17,10 @@ app.on('ready', function(){
     protocol:'file:',
     slashes: true
   }));
+// Quit app when closed
+mainWindow.on('closed', function(){
+  app.quit();
+});
 
 //Build menu from template
 const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
@@ -23,13 +28,37 @@ const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 Menu.setApplicationMenu(mainMenu);
 });
 
+// Handle create reset password window
+ function createResetPasswordWindow(){
+   //create new window
+   resetPasswordWindow = new BrowserWindow({
+     nodeIntegration : true,
+     width:300,
+     height:200,
+     title:'Reset Your Password'
+   });
+   //Load html into window
+   resetPasswordWindow.loadURL(url.format({
+     pathname: path.join(__dirname, 'resetPasswordWindow.html'),
+     protocol:'file:',
+     slashes: true
+   }));
+   // Garbage collection handle
+   resetPasswordWindow.on('close', function(){
+     resetPasswordWindow = null;
+   });
+ }
+
 //Create menu template
 const mainMenuTemplate = [
   {
     label: 'File',
     submenu:[
       {
-        label: 'Rest Password'
+        label: 'Reset Password',
+        click(){
+          createResetPasswordWindow();
+        }
       },
       {
         label: 'Quit',
@@ -41,3 +70,27 @@ const mainMenuTemplate = [
     ]
   }
 ];
+
+// If mac, add empty object to menu to change the default electron menu item
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({});
+}
+
+//Add developer tools item to menu if not in production mode
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer tools',
+    submenu:[
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ? command+I : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  });
+}
